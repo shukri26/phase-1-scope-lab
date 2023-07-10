cde6348775f4748c111f309c5b7a59e499cbc269
Tim <mohamud.hussein@student.moringaschool.com>
2023-07-10 07:26:00 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(customerName){
+    return bestCustomer = customerName;
+}
+
+const leastFavoriteCustomer = 'Andy';
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'Ben';
+}
